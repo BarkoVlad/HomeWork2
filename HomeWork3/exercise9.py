ed9406c18db1e4a9313b2729d09d89e048048e95
vladislavbarkovskij <barkovsky.vlad@gmail.com>
2022-10-19 21:37:01 +0300

    HomeWork3

--- HomeWork3/exercise9.py
+++ HomeWork3/exercise9.py
@@ -1,1 +1,9 @@
-word = input('Введите слово : ')
+word = input("Please enter a word : ")
+first = word[0]
+lenght = len(word)
+rest = word[1:lenght]
+if first != "a" and first != "e" and first != "i" and first != "o" and first != "u":
+    newword = rest + first + "ay"
+else:
+    newword = word + "way"
+print(newword.lower())
